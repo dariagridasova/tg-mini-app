a3dd85fce35345d2f9ed99ecdf019756360717e3
dariagridasova <dariagridasova.react@gmail.com>
2025-04-07 10:28:37 +0200

    commit for rebuild for netlify

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={onToggleButton}>toggle</button>
+      <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
