b63887728f81d017e320ceac46f065c8740ef191
dariagridasova <dariagridasova.react@gmail.com>
2025-04-08 17:00:35 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -16,14 +16,14 @@ const Form = () => {
         };
 
         tg.sendData(JSON.stringify(data));
-    }, [country, street, subject]);
+    }, [country, street, subject, tg]);
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData);
         return () => {
             tg.offEvent('mainButtonClicked', onSendData);
         }
-    }, [onSendData]);
+    }, [onSendData, tg]);
 
     useEffect(() => {
         tg.MainButton.setParams({
