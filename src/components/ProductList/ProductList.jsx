ccb476c55cafcd795680dc538a88cefacd0e6b20
dariagridasova <dariagridasova.react@gmail.com>
2025-04-09 16:18:27 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -100,7 +100,7 @@ const ProductList = () => {
 
     return (
         <div className={'list'}>
-            {ProductList.map(item => (
+            {products.map(item => (
                 <ProductItem
                     product={item}
                     onAdd={onAdd}
